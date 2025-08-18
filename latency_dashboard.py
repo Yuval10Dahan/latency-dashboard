ef0729ba3506a0d95391c50fee1c93db0bd1dec2
yuval_D <yuval_d@packetlight.com>
2025-08-18 14:55:26 +0300

    #4

--- latency_dashboard.py
+++ latency_dashboard.py
@@ -33,6 +33,7 @@ df = load_data()
 logo_path = os.path.join(os.path.dirname(__file__), 'Packetlight Logo.png')
 st.image(Image.open(logo_path), width=250)
 st.title("PacketLight - Latency Results")
+st.subheader("(The measurement calculated on a setup with 2 connected devices)")
 
 # --- Define renamed display columns ---
 display_columns_map = {
