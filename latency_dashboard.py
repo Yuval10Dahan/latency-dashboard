ab5f0921a8777eb5b25841b839b491bac4c8fe14
Yuval Dahan <Yuval_D@packetlight.com>
2025-12-11 16:00:55 +0200

    #4

--- latency_dashboard.py
+++ latency_dashboard.py
@@ -29,6 +29,29 @@ def load_data():
     # Drop unused columns
     # df = df.drop(columns=[col for col in ['step', 'id'] if col in df.columns])
     df = df.drop(columns=[col for col in ['step'] if col in df.columns])
+
+    desired_order = [
+        'id',
+        'product_name',
+        'datetime',
+        'serial_number',
+        'part_number',
+        'hardware_version',
+        'firmware_version',
+        'traffic_generator_application',
+        'system_mode',
+        'client_service_type',
+        'client_fec_mode',
+        'uplink_service_type',
+        'uplink_fec_mode',
+        'modulation_format',
+        'uplink_transceiver',   
+        'frame_size',
+        'result',
+    ]
+    # keep only columns that actually exist
+    df = df[[c for c in desired_order if c in df.columns]]
+    
     return df
 
 df = load_data()
